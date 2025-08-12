926a16896625dc180ce611cb748c1ad692653811
TwistedLlarmar <69968042+Future950@users.noreply.github.com>
2025-08-13 00:55:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://YOUR_GITHUB_USERNAME.github.io'
+    'https://Future950.github.io'
   ],
   methods: ['GET','POST','PUT','DELETE'],
   credentials: true
